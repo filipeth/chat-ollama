9f825e1a751407a1c0566b3028679355412a6d9a
Filipe Theodoro <ffatheodoro@gmail.com>
2024-08-08 14:49:46 -0300

    feat: ollama llm

---
+++ app/services/llms/__init__.py
@@ -0,0 +1,1 @@
+from app.services.llms.llms import load_llm
--- app/services/llms/llms.py
+++ app/services/llms/llms.py
@@ -1,2 +1,6 @@
-def load_llm():
-    pass
+from langchain_ollama.llms import OllamaLLM
+import os
+
+
+def load_llm(model=os.getenv('LLM_MODEL', 'llama3.1')):
+    return OllamaLLM(model=model, keep_alive=-1)
